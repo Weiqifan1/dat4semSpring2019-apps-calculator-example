6e5537674937e46707f32c6caf4f188d2dc8c3bd
Anders Kalhauge <anders@kalhauge.dk>
2019-02-14 22:44:28 +0100

    multibuttons

--- app/src/main/java/dk/cphbusiness/androidkotlinintro/Calculator9Activity.kt
+++ app/src/main/java/dk/cphbusiness/androidkotlinintro/Calculator9Activity.kt
@@ -2,11 +2,13 @@ package dk.cphbusiness.androidkotlinintro
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
 class Calculator9Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_calculator9)
     }
 }
